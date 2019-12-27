1c5cf432f54451a95d66fc8ef4f6cf4de49e4a94
TysonKerr <tysonkerr117@gmail.com>
2019-12-26 18:22:57 -0800

    set open_basedir to app root
    set in both htaccess and init.php

--- code/php/init.php
+++ code/php/init.php
@@ -3,5 +3,6 @@
 error_reporting(E_ALL);
 ini_set('auto_detect_line_endings', true);
 ini_set('zlib.output_compression', -1);
+ini_set('open_basedir', dirname(dirname(__DIR__)));
 ob_start();
 require dirname(dirname(__DIR__)) . '/code/php/general-definitions.php';
