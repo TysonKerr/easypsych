0a8e31c510b31fbdb020b5398dfdde19e95c7e91
TysonKerr <tysonkerr117@gmail.com>
2019-12-27 22:31:09 -0800

    Hid filter-age rejection message when preparing to submit trial
    also removed superfluous survey-proc.csv file

--- trial-types/filter-age/script.js
+++ trial-types/filter-age/script.js
@@ -1,5 +1,11 @@
+document.getElementById("rejection-message").style.display = "none";
+
 trial.onload = function() {
     let age = trial.get_last_response_value("Age");
     
-    if (age >= 18) trial.submit();
+    if (age >= 18) {
+        trial.submit();
+    } else {
+        document.getElementById("rejection-message").style.display = "block";
+    }
 };
