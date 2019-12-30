3778712d40b3a26123c281ec0320a8f4c516551e
TysonKerr <tysonkerr117@gmail.com>
2019-12-30 04:25:31 -0800

    made pause-experiment wait for data submission

--- trial-types/pause-experiment/script.js
+++ trial-types/pause-experiment/script.js
@@ -1,26 +1,42 @@
 "use strict";
 trial.onload = function() {
-    let last_submit_date = trial.get_last_response_value("Exp_Date");
-    let last_submit_timestamp = helper.get_timestamp_from_date(last_submit_date);
-    let seconds_since_last_submit = (Date.now() - last_submit_timestamp) / 1000;
-    let delay_required = trial.get_value("Settings");
-    let delay_left = delay_required - seconds_since_last_submit;
+    const delay_left = get_delay_left();
     
     if (delay_left <= 0) {
         trial.submit();
     } else {
-        let time_left_display = document.querySelector("#time-to-return");
-        
-        if (time_left_display) time_left_display.textContent = format_time(delay_left);
+        show_pause_message(delay_left);
     }
 };
 
+function get_delay_left() {
+    const last_submit_date = trial.get_last_response_value("Exp_Date");
+    const last_submit_timestamp = helper.get_timestamp_from_date(last_submit_date);
+    const seconds_since_last_submit = (Date.now() - last_submit_timestamp) / 1000;
+    const delay_required = trial.get_value("Settings");
+    return delay_required - seconds_since_last_submit;
+}
+
+function show_pause_message(delay_left) {
+    let time_left_display = document.querySelector("#time-to-return");
+    
+    if (time_left_display) time_left_display.textContent = format_time(delay_left);
+    
+    trial.wait_for_data_submission_to_complete()
+        .then(show_complete_message);
+}
+
+function show_complete_message() {
+    document.getElementById("data-submission-pending").style.display = "none";
+    document.getElementById("data-submission-complete").style.display = "block";
+}
+
 function format_time(seconds) {
-    let days = Math.floor(seconds / 86400);
+    const days = Math.floor(seconds / 86400);
     seconds %= 86400;
-    let hours = Math.floor(seconds / 3600);
+    const hours = Math.floor(seconds / 3600);
     seconds %= 3600;
-    let minutes = Math.floor(seconds / 60);
+    const minutes = Math.floor(seconds / 60);
     seconds %= 60;
     seconds = Math.min(59, Math.ceil(seconds));
     
