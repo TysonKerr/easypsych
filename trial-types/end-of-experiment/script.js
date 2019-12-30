77bdfaa4f21fde9e5516905e44d1bd16eff040b5
TysonKerr <tysonkerr117@gmail.com>
2019-12-30 04:16:54 -0800

    removing testing code from end-of-experiment

--- trial-types/end-of-experiment/script.js
+++ trial-types/end-of-experiment/script.js
@@ -1,6 +1,5 @@
 trial.onload = () => 
     trial.wait_for_data_submission_to_complete()
-    .then(() => new Promise(resolve => setTimeout(resolve, 2000)))
     .then(show_complete_message);
 
 function show_complete_message() {
